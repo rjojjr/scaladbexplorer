dbbec3f4edd1533ffb45bd0144d940eb3ec8609f
Robert Kirchner JR <robert.kirchner@wwt.com>
2019-12-06 08:34:30 -0600

    add reading schema

---
+++ src/main/scala/com/ks/scala/dbexplorer/schema/Readings.scala
@@ -0,0 +1,17 @@
+package com.ks.scala.dbexplorer.schema
+
+import slick.ast.ColumnOption.AutoInc
+import slick.lifted.Tag
+import slick.model.Table
+
+case class Reading(id: Option[Long], readingTime: Long, temperatire: Int, humidity: Int, roomname: String)
+
+class Readings(tag: Tag) extends Table[(Long, Long, Int, Int, String)](tag, "readings") {
+
+  def id = column[Long]("id", O.PrimaryKey, AutoInc)
+  def readingTime = column[Long]("reading_time")
+  def temperature = column[Int]("temperature")
+  def humidity = column[Int]("humidity")
+  def roomname = column[String]("room_name")
+
+}
