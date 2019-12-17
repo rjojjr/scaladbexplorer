bf99e4e15bc7b872cc3678b04f975e5948c2c576
Robert Kirchner JR <robert.kirchner@wwt.com>
2019-12-17 12:21:00 -0600

    fix insert

--- src/main/scala/com/ks/scala/dbexplorer/dbobjects/TestDB.scala
+++ src/main/scala/com/ks/scala/dbexplorer/dbobjects/TestDB.scala
@@ -2,6 +2,8 @@ package com.ks.scala.dbexplorer.dbobjects
 
 import com.ks.scala.dbexplorer.dbitems.AppUser
 import com.ks.scala.dbexplorer.schema.AppUsersSQL
+import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.duration._
 
 object TestDB {
 
@@ -9,16 +11,24 @@ object TestDB {
     val time = System.currentTimeMillis()
     val sql = new AppUsersSQL()
     sql insertAppUser user
-    //println("User " + user.username + " inserted at " + time)
+    println("User " + user.username + " inserted at " + time)
   }
 
   def find() = {
     val time = System.currentTimeMillis()
     val sql = new AppUsersSQL()
     val sqluser = sql getUserByUsername "testuser1"
-    val appUser: AppUser = sqluser.asInstanceOf[AppUser]
-    //println("User " + appUser.username + " found at " + time)
+    val vector = sqluser
+    var appUser: AppUser = vector(0)
+    println("User " + appUser.username + " found at " + time)
     appUser
+  }
+
+  def main(args: Array[String]): Unit ={
+    val time = System.currentTimeMillis()
+    val user = AppUser(1, time, "test", "user", "testuser1", "SomePW55#", false)
+    create(user)
+    find()
   }
 
 }
--- src/main/scala/com/ks/scala/dbexplorer/schema/AppUsersSQL.scala
+++ src/main/scala/com/ks/scala/dbexplorer/schema/AppUsersSQL.scala
@@ -1,12 +1,13 @@
 package com.ks.scala.dbexplorer.schema
 
+import akka.actor.ActorSystem
 import com.ks.scala.dbexplorer.dbitems.AppUser
 import com.typesafe.config.ConfigFactory
 import slick.dbio.DBIO
 import slick.jdbc.GetResult
 import slick.jdbc.MySQLProfile.api._
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration._
 
 class AppUsersSQL {
@@ -17,10 +18,11 @@ class AppUsersSQL {
   implicit val userConverter =
     GetResult[AppUser](r => AppUser(r.<<, r.<<, r.<<, r.<<, r.<<, r.<<, r.<<))
 
-  def insertAppUser(user: AppUser): DBIO[Int] =
-    // Insert some suppliers
-    sqlu"insert into app_users values(${user.id}, ${user.creationTime}, ${user.firstname}, ${user.lastname}, ${user.password}, ${user.admin})"
-
+  def insertAppUser(user: AppUser) = {
+    val rawSQL =
+      sqlu"insert into app_users (id, creation_time, first_name, last_name, user_name, password, admin) values(${user.id}, ${user.creationTime}, ${user.firstname}, ${user.lastname}, ${user.username}, ${user.password}, ${user.admin})"
+    Await.result(db.run(rawSQL), 20.seconds)
+  }
   def insertAppUsers(users: Seq[AppUser]) = {
     users.foreach(u => {
       insertAppUser(u)
@@ -37,21 +39,18 @@ class AppUsersSQL {
   }
 
   def getUserByUsername(userName: String) = {
-    //val rawSQL =
+    val rawSQL =
     sql"""select u.id, u.creation_time, u.first_name, u.last_name, u.user_name, u.password, u.admin from app_users u where u.user_name = ${userName}"""
       .as[AppUser]
-    //Await.result(db.run(rawSQL), 30000.millis).asInstanceOf[AppUser]
-    implicit val userConverter =
-      GetResult[AppUser](r => AppUser(r.<<, r.<<, r.<<, r.<<, r.<<, r.<<, r.<<))
+    Await.result(db.run(rawSQL), 20.seconds)
+
   }
 
-  def getUserByUsernameAndPassword(userName: String, password: String) = {
-    //val rawSQL =
+  def getUserByUsernameAndPassword(userName: String, password: String)(implicit ec: ExecutionContext, ac: ActorSystem) = {
+    val rawSQL =
     sql"""select u.id, u.creation_time, u.first_name, u.last_name, u.user_name, u.password, u.admin from app_users u where u.user_name = ${userName} and u.password = $password}"""
       .as[AppUser]
-    //Await.result(db.run(rawSQL), 30000.millis).asInstanceOf[AppUser]
-    implicit val userConverter =
-      GetResult[AppUser](r => AppUser(r.<<, r.<<, r.<<, r.<<, r.<<, r.<<, r.<<))
+    val result = db.run(rawSQL)
   }
 
 }
