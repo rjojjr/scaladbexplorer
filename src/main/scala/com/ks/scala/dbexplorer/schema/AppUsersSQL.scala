d4ca50f126bcd4aa8989abc85fe4a96643bda065
Robert Kirchner JR <robert.kirchner@wwt.com>
2019-12-17 12:53:12 -0600

    reformat sql

--- src/main/scala/com/ks/scala/dbexplorer/schema/AppUsersSQL.scala
+++ src/main/scala/com/ks/scala/dbexplorer/schema/AppUsersSQL.scala
@@ -3,12 +3,11 @@ package com.ks.scala.dbexplorer.schema
 import akka.actor.ActorSystem
 import com.ks.scala.dbexplorer.dbitems.AppUser
 import com.typesafe.config.ConfigFactory
-import slick.dbio.DBIO
 import slick.jdbc.GetResult
 import slick.jdbc.MySQLProfile.api._
 
-import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration._
+import scala.concurrent.{Await, ExecutionContext}
 
 class AppUsersSQL {
 
@@ -21,7 +20,7 @@ class AppUsersSQL {
   def insertAppUser(user: AppUser) = {
     val rawSQL =
       sqlu"insert into app_users (id, creation_time, first_name, last_name, user_name, password, admin) values(${user.id}, ${user.creationTime}, ${user.firstname}, ${user.lastname}, ${user.username}, ${user.password}, ${user.admin})"
-    Await.result(db.run(rawSQL), 20.seconds)
+    db.run(rawSQL)
   }
 
   def insertAppUsers(users: Seq[AppUser]) = {
@@ -31,27 +30,31 @@ class AppUsersSQL {
   }
 
   def deleteAppUser(user: AppUser) = {
-    sql"""delete from app_users u where u.id = ${user.id}"""
+    val rawSQL =
+      sqlu"delete from app_users u where u.id = ${user.id}"
+    db.run(rawSQL)
   }
 
   def getUserSessionId(user: AppUser) = {
-    sql"""select s.session_id from appuser_sessions s where s.user_id = ${user.id}"""
-      .as[(Long)]
+    val rawSQL =
+      sql"""select s.session_id from appuser_sessions s where s.user_id = ${user.id}"""
+        .as[(Long)]
+    Await.result(db.run(rawSQL), 5.seconds)
   }
 
   def getUserByUsername(userName: String) = {
     val rawSQL =
-    sql"""select u.id, u.creation_time, u.first_name, u.last_name, u.user_name, u.password, u.admin from app_users u where u.user_name = ${userName}"""
-      .as[AppUser]
-    Await.result(db.run(rawSQL), 20.seconds)
+      sql"""select u.id, u.creation_time, u.first_name, u.last_name, u.user_name, u.password, u.admin from app_users u where u.user_name = ${userName}"""
+        .as[AppUser]
+    Await.result(db.run(rawSQL), 5.seconds)
 
   }
 
   def getUserByUsernameAndPassword(userName: String, password: String)(implicit ec: ExecutionContext, ac: ActorSystem) = {
     val rawSQL =
-    sql"""select u.id, u.creation_time, u.first_name, u.last_name, u.user_name, u.password, u.admin from app_users u where u.user_name = ${userName} and u.password = $password}"""
-      .as[AppUser]
-    val result = db.run(rawSQL)
+      sql"""select u.id, u.creation_time, u.first_name, u.last_name, u.user_name, u.password, u.admin from app_users u where u.user_name = ${userName} and u.password = $password}"""
+        .as[AppUser]
+    Await.result(db.run(rawSQL), 5.seconds)
   }
 
 }
